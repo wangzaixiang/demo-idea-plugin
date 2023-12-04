d964f3f92a69f8789273ca6b1239fff9bc4c6356
GitHub Action <action@github.com>
2023-12-04 08:29:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangzaixiang/demoideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/wangzaixiang/demoideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wangzaixiang.demoideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
